1b0d73fc60abbd81c2ab5acff3d7cbc6019fecf6
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-05-12 01:27:00 +0900

    feat: config layout : markdown to html

--- src/View/Layout.elm
+++ src/View/Layout.elm
@@ -1,9 +1,17 @@
 module View.Layout exposing (view)
 
-import Html.Styled exposing (a, div, footer, main_, nav, p, span, text)
-import Html.Styled.Attributes exposing (attribute, class, href, id)
-import Html.Styled.Events exposing (onClick)
-import Site exposing (config)
+{-| This module configures the layout.
+
+@docs view
+
+-}
+
+import Css
+import Css.Global exposing (global)
+import Html.Styled as Html
+import Html.Styled.Attributes as Attr
+import Html.Styled.Events as Evt
+import Site
 
 
 view :
@@ -12,13 +20,17 @@ view :
         , onOpenMenu : msg
         , onCloseMenu : msg
     }
-    -> List (Html.Styled.Html msg)
-    -> Html.Styled.Html msg
+    -> List (Html.Html msg)
+    -> Html.Html msg
 view config body =
-    div
+    Html.div
         []
-        [ navbar_ config
-        , main_ [] body
+        [ global
+            [ Css.Global.body
+                [ Css.fontFamilies [ Css.qt "Fira Mono", Css.qt "monospace" ] ]
+            ]
+        , navbar_ config
+        , Html.main_ [] body
         , footer_
         ]
 
@@ -29,10 +41,10 @@ navbar_ :
         , onOpenMenu : msg
         , onCloseMenu : msg
     }
-    -> Html.Styled.Html msg
+    -> Html.Html msg
 navbar_ config =
     let
-        state =
+        menuState =
             if config.showMenu then
                 " is-active"
 
@@ -46,44 +58,59 @@ navbar_ config =
             else
                 config.onOpenMenu
     in
-    nav
-        [ class "navbar" ]
-        [ div
-            [ class "navbar-brand" ]
-            [ a
-                [ class "navbar-item", href "/" ]
-                [ text Site.title ]
-            , div
-                [ class ("navbar-burger burger" ++ state)
-                , attribute "aria-expanded" "false"
-                , attribute "data-target" "navMenu"
-                , onClick menuMsg
+    Html.nav
+        [ Attr.class "navbar" ]
+        [ Html.div
+            [ Attr.class "container is-max-desktop" ]
+            [ Html.div
+                [ Attr.class "navbar-brand" ]
+                [ Html.a
+                    [ Attr.class "navbar-item is-size-4 has-Html.text-weight-bold", Attr.href "/" ]
+                    [ Html.text Site.title ]
+                , Html.div
+                    [ Attr.class ("navbar-burger burger" ++ menuState)
+                    , Attr.attribute "aria-expanded" "false"
+                    , Attr.attribute "data-target" "navMenu"
+                    , Evt.onClick menuMsg
+                    ]
+                    [ Html.span [] []
+                    , Html.span [] []
+                    , Html.span [] []
+                    ]
                 ]
-                [ span [] []
-                , span [] []
-                , span [] []
-                ]
-            ]
-        , div
-            [ class ("navbar-menu" ++ state), id "navMenu" ]
-            [ div
-                [ class "navbar-end" ]
-                [ a
-                    [ class "navbar-item", href "/blog" ]
-                    [ text "Blog" ]
-                , a
-                    [ class "navbar-item", href "/about" ]
-                    [ text "About" ]
+            , Html.div
+                [ Attr.class ("navbar-menu" ++ menuState), Attr.id "navMenu" ]
+                [ Html.div
+                    [ Attr.class "navbar-end" ]
+                    [ Html.a
+                        [ Attr.class "navbar-item", Attr.href "/blog" ]
+                        [ Html.text "Blog" ]
+                    , Html.a
+                        [ Attr.class "navbar-item", Attr.href "/about" ]
+                        [ Html.text "About" ]
+                    ]
                 ]
             ]
         ]
 
 
-footer_ : Html.Styled.Html msg
+footer_ : Html.Html msg
 footer_ =
-    footer
-        [ class "footer" ]
-        [ div
-            [ class "content has-text-centered" ]
-            [ p [] [ text "© 2022 tkoyasak" ] ]
+    Html.footer
+        [ Attr.class "footer" ]
+        [ Html.div
+            [ Attr.class "container is-max-desktop" ]
+            [ Html.div
+                [ Attr.class "content has-text-right" ]
+                [ Html.p []
+                    [ Html.text "Powered by "
+                    , Html.a [ Attr.href "https://elm-pages.com/" ] [ Html.text "elm-pages" ]
+                    , Html.text ". Source is "
+                    , Html.a [ Attr.href "https://github.com/tkoyasak/tkoyasak.dev" ] [ Html.text "here" ]
+                    , Html.text "."
+                    , Html.br [] []
+                    , Html.text "© 2022 tkoyasak"
+                    ]
+                ]
+            ]
         ]
