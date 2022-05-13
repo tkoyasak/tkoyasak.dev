64ef76b7a8d4268986b844a5853ad02b06885c05
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-05-13 16:09:03 +0900

    feat: /about page and layout

---
+++ content/about.md
@@ -0,0 +1,4 @@
+### Contacts
+- [Github](https://github.com/tkoyasak)
+- [Twitter](https://twitter.com/tkoyasak)
+- [AtCoder](https://atcoder.jp/users/tkoyasak)
---
+++ src/Page/About.elm
@@ -0,0 +1,75 @@
+module Page.About exposing (..)
+
+import DataSource exposing (DataSource)
+import DataSource.File
+import Head
+import Head.Seo as Seo
+import Html.Styled as Html
+import Page exposing (Page, StaticPayload)
+import Pages.PageUrl exposing (PageUrl)
+import Shared
+import Site
+import View exposing (View)
+import View.Markdown
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    {}
+
+
+page : Page RouteParams Data
+page =
+    Page.single
+        { head = head
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+data : DataSource Data
+data =
+    DataSource.File.rawFile "content/about.md"
+        |> DataSource.map View.Markdown.toHtml
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head _ =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = Site.title
+        , image =
+            { url = Site.iconUrl
+            , alt = Site.title ++ " icon"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = Site.description
+        , locale = Nothing
+        , title = "About | " ++ Site.title
+        }
+        |> Seo.website
+
+
+type alias Data =
+    Html.Html Msg
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view _ _ static =
+    { title = "About"
+    , body = [ static.data ]
+    }
