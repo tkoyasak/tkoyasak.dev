d0c449a983c2693ea7f9a648b7fb51d9544b9461
tkoyasak <72780548+tkoyasak@users.noreply.github.com>
2022-05-23 20:18:41 +0900

    feat: display the number of articles for each tag

--- src/Data/Tags.elm
+++ src/Data/Tags.elm
@@ -1,7 +1,9 @@
-module Data.Tags exposing (Metadata, getAllTags, metadataDecoder)
+module Data.Tags exposing (Metadata, getAllTags, getReferencedTags, metadataDecoder)
 
 import Data.Api exposing (requestContent)
 import DataSource
+import Dict
+import Dict.Extra as Dict
 import OptimizedDecoder as Decoder
 
 
@@ -23,3 +25,33 @@ metadataDecoder =
     Decoder.map2 Metadata
         (Decoder.field "id" Decoder.string)
         (Decoder.field "name" Decoder.string)
+
+
+getReferencedTags : DataSource.DataSource (List ( String, Int ))
+getReferencedTags =
+    requestContent
+        "posts"
+        (Decoder.field "contents"
+            (Decoder.list
+                (Decoder.field "tags"
+                    (Decoder.list (Decoder.field "name" Decoder.string))
+                )
+            )
+        )
+        |> DataSource.map List.concat
+        |> DataSource.map Dict.frequencies
+        |> DataSource.map Dict.toList
+        |> DataSource.map (List.sortWith descendingOrder)
+
+
+descendingOrder : ( a, Int ) -> ( a, Int ) -> Order
+descendingOrder a b =
+    case compare (Tuple.second a) (Tuple.second b) of
+        LT ->
+            GT
+
+        EQ ->
+            EQ
+
+        GT ->
+            LT
